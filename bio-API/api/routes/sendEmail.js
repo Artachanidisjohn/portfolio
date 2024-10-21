bf89c9aa25c2cd3fbbf6dde49c6eaac1f39863f7
Artachanidis Ioannis <82318135+Artachanidisjohn@users.noreply.github.com>
2024-10-21 12:08:09 +0300

    Delete bio-API directory

--- bio-API/api/routes/sendEmail.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const emailController = require('../controllers/emailController');
-
-// Δημιουργία του POST route για την αποστολή email
-router.post('/send-email', emailController.sendEmailController);
-
-module.exports = router;
